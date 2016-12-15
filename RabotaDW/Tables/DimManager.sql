8d3582c1c23198e7cdc7c5e765af253b4b154099
AnastasiyaG <AnastasiyaG@RUA5141>
2016-12-15 15:30:49 +0200

    add DWHAzure_Dimension.dtsx to RabotaETL, dbo.DimManager to RabotaDW

---
+++ RabotaDW/Tables/DimManager.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[DimManager]
+(
+	[ManagerKey] [int] IDENTITY(1,1) NOT NULL,
+    [ManagerId] INT NOT NULL, 
+    [Name] NVARCHAR(500) NULL, 
+    [Email] NVARCHAR(256) NULL, 
+    [DepartmentId] TINYINT NULL, 
+    [AddDate] DATETIME NULL, 
+    [DeleteDate] DATETIME NULL, 
+    [STMId] INT NULL, 
+    [SMId] INT NULL, 
+    [IsLoyaltyGroup] BIT NOT NULL, 
+    [StartDate] DATETIME NOT NULL, 
+    [EndDate] DATETIME NULL 
+)
