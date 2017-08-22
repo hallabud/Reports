0e3e4a031d7a6842d78b8c27a13cec5b7d86a1f5
Mykhailo Abdulakh <michael.abdulakh@gmail.com>
2017-08-22 11:35:22 +0300

    BI-684

---
+++ RabotaDW/Tables/DimOrder.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[DimOrder]
+(
+	[OrderID] INT NOT NULL, 
+    [NotebookID] INT NOT NULL, 
+    [LoginEmail_OrderOwner] NVARCHAR(100) NOT NULL, 
+    [CatalogPriceSum] MONEY NOT NULL, 
+    [ClientPriceSum] MONEY NOT NULL, 
+    [WaitActivationDate] SMALLDATETIME NOT NULL, 
+    [AddDate] SMALLDATETIME NOT NULL, 
+    [UpdateDate] SMALLDATETIME NOT NULL, 
+    CONSTRAINT [PK_DimOrder] PRIMARY KEY ([OrderID]), 
+    CONSTRAINT [FK_DimOrder_DimNotebookCompany] FOREIGN KEY (NotebookID) REFERENCES DimNotebookCompany(NotebookID) 
+)
--- RabotaDW/Tables/FactRecognizedRevenue.sql
+++ RabotaDW/Tables/FactRecognizedRevenue.sql
@@ -2,8 +2,12 @@
 (
 	[DateKey] INT NOT NULL, 
     [NotebookID] INT NOT NULL, 
+	OrderID INT NOT NULL,
 	[OrderServiceID] SMALLINT NOT NULL, 
     [RecognizedRevenueAmount] MONEY NOT NULL, 
+	CONSTRAINT [PK_FactRecognizedRevenue] PRIMARY KEY ([DateKey], [OrderID], [NotebookID], [OrderServiceID]),
     CONSTRAINT [FK_FactRecognizedRevenue_DimNotebookCompany] FOREIGN KEY (NotebookID) REFERENCES DimNotebookCompany(NotebookID), 
-    CONSTRAINT [FK_FactRecognizedRevenue_DimService] FOREIGN KEY (OrderServiceID) REFERENCES DimService(OrderServiceID) 
+    CONSTRAINT [FK_FactRecognizedRevenue_DimService] FOREIGN KEY (OrderServiceID) REFERENCES DimService(OrderServiceID), 
+    CONSTRAINT [FK_FactRecognizedRevenue_DimOrder] FOREIGN KEY (OrderID) REFERENCES DimOrder(OrderID), 
+    CONSTRAINT [FK_FactRecognizedRevenue_DimDate] FOREIGN KEY (DateKey) REFERENCES DimDate(DateKey)
 )
